2ff0965a86adccd2ecd6b45e3e533ef4dc40250a
brittadam <britt.adam@hotmail.com>
2023-11-28 14:31:14 +0100

    fix: Update WebSocket URL in Chat.vue

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -8,7 +8,7 @@ let socket= null;
 onMounted(()=>{
     // console.log('mounted');
     //connect to web sockets servers
-    socket= new WebSocket('ws://localhost:3000/primus');
+    socket= new WebSocket('wss://discord-chat-phi.vercel.app//primus');
 
 
     //listen for messages from web socket server
